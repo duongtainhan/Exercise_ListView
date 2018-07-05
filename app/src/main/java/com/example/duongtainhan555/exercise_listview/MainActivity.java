b1aecd468322a8494c1da334dd9e82e8eab6edfa
Dương Tài Nhân <15521044@gm.uit.edu.vn>
2018-07-05 16:23:41 +0700

    Add value in array list

--- app/src/main/java/com/example/duongtainhan555/exercise_listview/MainActivity.java
+++ app/src/main/java/com/example/duongtainhan555/exercise_listview/MainActivity.java
@@ -2,12 +2,41 @@ package com.example.duongtainhan555.exercise_listview;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.ArrayAdapter;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.ListView;
+
+import java.util.ArrayList;
 
 public class MainActivity extends AppCompatActivity {
 
+    ListView lv;
+    Button btnAdd;
+    EditText edTxt;
+    String s ="";
+    ArrayList<String> arrayList;
+    ArrayAdapter arrayAdapter;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        lv = findViewById(R.id.lv);
+        btnAdd = findViewById(R.id.btnAdd);
+        edTxt = findViewById(R.id.edTxt);
+        arrayList = new ArrayList<>();
+
+        btnAdd.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                s = edTxt.getText().toString();
+
+                arrayList.add(s);
+                arrayAdapter = new ArrayAdapter(MainActivity.this,android.R.layout.simple_list_item_1,arrayList);
+                lv.setAdapter(arrayAdapter);
+            }
+        });
     }
 }
